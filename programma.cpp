5469a63ad63ee39f50952d9a2972ad489a4d6ecd
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-02-24 16:42:25 +0100

    manca correzione punto 4

--- programma.cpp
+++ programma.cpp
@@ -1,67 +1,107 @@
-// file matricole.txt
-//  11485 Marius
-//  11486 Andrei
-//  11487 Marco
-// file straordinari
-//  11485 10
-//  11486 20
-//  11487 30
-// file stipendi
-//  11485 1000
-//  11486 2000
-//  11487 3000
-
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-string nome = "";
-
-void findNameByCod(string cod)
+string cerca(string nomefile, string cod)
 {
+    string risultato = "";
 
-    ifstream fileMatricole("matricola.txt");
-    if (fileMatricole.is_open())
+    ifstream fileAperto(nomefile);
+    if (fileAperto.is_open())
     {
-        cout << "File matricole.txt aperto correttamente" << endl;
         string line;
-
-        while (getline(fileMatricole, line))
+        while (getline(fileAperto, line))
         {
+
             for (int i = 0; i < 6; i++)
             {
                 if (line[i] != cod[i])
                 {
                     break;
                 }
-                else if (i == 4)
+                else if (i == 4) // ultimo carattere della matricola
                 {
                     for (int j = 6; j < line.length(); j++)
                     {
-                        nome += line[j];
+                        risultato += line[j];
                     }
                 }
+                else
+                {
+                }
             }
-        }
-         // cout << "La matricola: " << cod << " corrisponde al nome: " << nome << endl;
+        } fileAperto.close();
     }
     else
     {
-        cout << "Impossibile aprire il file matricole.txt" << endl;
+        cout << "Impossibile aprire il file .txt" << endl;
     }
 
-    cout << "La matricola " << cod << " corrisponde al nome: " << nome << endl;
+    return risultato;
+}
+
+void findNameByCod(string cod)
+{
+    string risultato = cerca("matricola.txt", cod);
+    cout << "La matricola: " << cod << " corrisponde al nome: " << risultato << endl;
 }
 
 void findSalaryByCod(string cod)
 {
-    cout << "sono findSalaryByCod" << endl;
+    cout << "matricola: " << cod << " stipendio: " << cerca("stipendio.txt", cod) << endl;
 }
 
-void findSalaryByName(string name)
+void findStraordinaryByCod(string cod)
 {
-    cout << "sono findSalaryByName" << endl;
+    cout << "matricola: " << cod << " straordinario: " << cerca("straordinari.txt", cod) << endl;
+}
+
+void findSalaryByName(string nome)
+{
+    ifstream fileAperto("matricola.txt");
+    string cod = "";
+    if (fileAperto.is_open())
+    {
+        string line;
+        while (getline(fileAperto, line))
+        {
+
+            for (int i = 6; i < line.length(); i++)
+            {
+                if (line[i] != nome[i])
+                {
+                    break;
+                }
+                else if (i == 4) // ultimo carattere della matricola
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        cod += line[j];
+                    }
+                }
+                else
+                {
+                }
+            }
+        }
+    }
+    else
+    {
+        cout << "Impossibile aprire il file .txt" << endl;
+    }
+
+    int stipendioValore = stoi(cerca("stipendio", cod));
+
+    cout << "Il nome: " << nome << " corrisponde alla matricola: " << cod << " e al stipendio: " << stipendioValore << endl;
+}
+void findSalaryAndStraordinaryByCod(string cod)
+{
+    int a = stoi(cerca("stipendio.txt", cod));         // richiamo il procedimento della funzione che calcola lo stipendio dalla matricola
+    int b = stoi(cerca("straordinari.txt", cod)) * 35; // richiamo il procedimento della funzione che calcola gli straordinari dalla matricola e lo moltiplico per 35 per avere le ore tradotte in soldi
+    int c = a + b;                                     // dopo avere trasformato in intero i risultati li posso sommare per avere il calcolo totale del guadagno (stipendio con straordinari)
+    cout << "matricola: " << cod << " il guadagno totale e' " << c << endl;
 }
 
 int main()
@@ -72,18 +112,20 @@ int main()
     while (flag)
     {
         cout << "------------------------------------------------------" << endl;
-
+        cout << "Inserisci il numero della funzionalità che vuoi attivare" << endl;
         cout << "1. Matricola -> Nome" << endl;
         cout << "2. Matricola -> Stipendio" << endl;
-        cout << "3. Nome -> Stipendio" << endl;
-        cout <<"inserisci la matricola"<< endl;
-        int scelta = 1;
-        // cin >> scelta;
-
+        cout << "3. Matricola -> Straordinari" << endl;
+        cout << "4. Nome -> Stipendio" << endl;
+        cout << "5. Matricola -> Stipendio + straordinari (35h)" << endl;
+        cout << "0. EXIT" << endl;
+        int scelta;
+        cin >> scelta;
+        cout << "Inserisci quello che vuoi cercare";
         string input;
         cin >> input;
 
-        if (scelta < 1 || scelta > 3)
+        if (scelta < 0 || scelta > 5) // DA MODIFICARE IN CASO DI AGGIUNTE NEL MENU
         {
             cout << "Scelta non valida" << endl;
         }
@@ -92,16 +134,25 @@ int main()
             if (scelta == 1 && input.length() == 5)
             {
                 findNameByCod(input);
-                flag = false;
             }
             else if (scelta == 2 && input.length() == 5)
             {
                 findSalaryByCod(input);
-                flag = false;
             }
             else if (scelta == 3)
             {
+                findStraordinaryByCod(input);
+            }
+            else if (scelta == 4)
+            {
                 findSalaryByName(input);
+            }
+            else if (scelta == 5)
+            {
+                findSalaryAndStraordinaryByCod(input);
+            }
+            else if (scelta == 0)
+            {
                 flag = false;
             }
             else
