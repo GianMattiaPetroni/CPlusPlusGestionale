2089c99e0cf3880c67f70a25fda16a92d864c48e
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-02-24 12:38:01 +0100

    programma finito

--- programma.cpp
+++ programma.cpp
@@ -16,41 +16,42 @@
 
 using namespace std;
 
+string nome = "";
+
 void findNameByCod(string cod)
 {
-    string nome = "";
-    // file matricole.txt
-    //  11485 Marius
-    //  11486 Andrei
-    //  11487 Marco
 
     ifstream fileMatricole("matricola.txt");
     if (fileMatricole.is_open())
     {
         cout << "File matricole.txt aperto correttamente" << endl;
         string line;
+
         while (getline(fileMatricole, line))
         {
-            for(int i=0; i<6 ; i++)
+            for (int i = 0; i < 6; i++)
             {
-                if(line[i] != cod[i])
+                if (line[i] != cod[i])
                 {
                     break;
                 }
-                else if(i == 5)
+                else if (i == 4)
                 {
-                    nome = "File"; //ho trovato, mi devo mettere il nome in nome
-                    // senza usare la funzione substr
+                    for (int j = 6; j < line.length(); j++)
+                    {
+                        nome += line[j];
+                    }
                 }
             }
-        } cout << "La matricola: " << cod << " corrisponde al nome: " << nome << endl;
+        }
+         // cout << "La matricola: " << cod << " corrisponde al nome: " << nome << endl;
     }
     else
     {
         cout << "Impossibile aprire il file matricole.txt" << endl;
     }
 
-    cout << "La matricola: " << cod << " corrisponde al nome: " << nome << endl;
+    cout << "La matricola " << cod << " corrisponde al nome: " << nome << endl;
 }
 
 void findSalaryByCod(string cod)
@@ -71,13 +72,14 @@ int main()
     while (flag)
     {
         cout << "------------------------------------------------------" << endl;
-        cout << "Inserisci il numero della funzionalità che vuoi attivare" << endl;
+
         cout << "1. Matricola -> Nome" << endl;
         cout << "2. Matricola -> Stipendio" << endl;
         cout << "3. Nome -> Stipendio" << endl;
-        int scelta;
-        cin >> scelta;
-        cout << "Inserisci quello che vuoi cercare";
+        cout <<"inserisci la matricola"<< endl;
+        int scelta = 1;
+        // cin >> scelta;
+
         string input;
         cin >> input;
 
